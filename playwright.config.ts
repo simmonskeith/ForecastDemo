3303f0f38f43c1122f83f53610126acd3d0ec96e
Keith Simmons <ksimmons2@gmail.com>
2026-06-02 21:05:34 -0400

    Fixed  test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,13 +24,25 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+  
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
   },
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+
+  },
+  
+  // use: {
+  //   /* Base URL to use in actions like `await page.goto('')`. */
+  //   // baseURL: 'http://localhost:3000',
+
+  //   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  //   trace: 'on-first-retry',
+  // },
 
   /* Configure projects for major browsers */
   projects: [
